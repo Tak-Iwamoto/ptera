ebde4488f6fd5692ec0f792bb651dfb8b72e00df
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-05-21 00:13:10 +0900

    add main class

--- datetime.ts
+++ datetime.ts
@@ -1,24 +1,54 @@
-import { Timezone } from "./types.ts";
-import { zonedTime } from "./zoned_time.ts";
+import { Timezone } from "./types.ts"
+import { zonedTime } from "./zoned_time.ts"
 
 function newInstance(date: Date): DD {
-  return new DD(date);
+  return new DD(date)
+}
+
+type Config = {
+  tz: Timezone;
 }
 
 export default class DD {
-  #ddDateTime: Date;
+  #ddDateTime: Date
 
-  constructor(date: Date | number | string) {
+  constructor(date: Date | number | string, config?: Config) {
     this.#ddDateTime = date instanceof Date
       ? new Date(date.getTime())
-      : new Date(date);
+      : new Date(date)
   }
 
-  getTime() {
-    return this.#ddDateTime.getTime();
+  getDate() {
+    return this.#ddDateTime
   }
 
-  tz(tz: Timezone) {
-    return newInstance(zonedTime(this.#ddDateTime, tz));
+  getTime(): number {
+    return this.#ddDateTime.getTime()
+  }
+
+  tz(tz: Timezone): DD {
+    return newInstance(zonedTime(this.#ddDateTime, tz))
+  }
+
+  startOfDay(): Date {
+    const clone = newInstance(this.#ddDateTime)
+    clone.#ddDateTime.setUTCHours(0, 0, 0, 0)
+    return clone.#ddDateTime
+  }
+
+  endOfDay(): Date {
+    const clone = newInstance(this.#ddDateTime)
+    clone.#ddDateTime.setUTCHours(23, 59, 59, 999)
+    return clone.#ddDateTime
+  }
+
+  startOfMonth(): Date {
+    const clone = newInstance(this.#ddDateTime)
+    clone.#ddDateTime.setUTCDate(1)
+    return clone.#ddDateTime
+  }
+
+  endOfMonth(): Date {
+    return newInstance(new Date(this.#ddDateTime.getUTCFullYear(), this.#ddDateTime.getUTCMonth() + 1, 0)).#ddDateTime
   }
 }
---
+++ datetime_test.ts
@@ -0,0 +1,9 @@
+import Datetime from "./datetime.ts";
+
+const dd = new Datetime(new Date())
+
+console.log(dd.getDate())
+console.log(dd.startOfDay())
+console.log(dd.endOfDay())
+console.log(dd.startOfMonth())
+console.log(dd.endOfMonth())
