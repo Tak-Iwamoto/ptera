01b31f54226db25a9aaf063dcbbd7eb1c059c63c
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-05-22 17:35:35 +0900

    fix offset name

--- local_time.ts
+++ local_time.ts
@@ -1,10 +1,10 @@
-import { MILLISECONDS_IN_MINUTE } from "./constants.ts"
-import { TimeStamp } from "./types.ts"
+import { MILLISECONDS_IN_MINUTE } from "./constants.ts";
+import { TimeStamp } from "./types.ts";
 
 export function getLocalName(): string {
-  return new Intl.DateTimeFormat().resolvedOptions().timeZone
+  return new Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
-export function getLocalMillisecondsOffset(timeStamp: TimeStamp): number {
-  return -new Date(timeStamp).getTimezoneOffset() * MILLISECONDS_IN_MINUTE
+export function getLocalOffset(timeStamp: TimeStamp): number {
+  return -new Date(timeStamp).getTimezoneOffset() * MILLISECONDS_IN_MINUTE;
 }
--- timezone_offset_test.ts
+++ timezone_offset_test.ts
@@ -1,30 +1,30 @@
-import { millisecondsOffset } from "./timezone_offset.ts";
+import { tzOffset } from "./timezone_offset.ts";
 import { assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
 import { MILLISECONDS_IN_HOUR } from "./constants.ts";
 
-Deno.test("millisecondsOffset Asia/Tokyo", () => {
-  const offset = millisecondsOffset(
+Deno.test("tzOffset Asia/Tokyo", () => {
+  const offset = tzOffset(
     new Date("2021-05-13T12:15:30Z"),
     "Asia/Tokyo",
   );
   assertEquals(offset, 9 * MILLISECONDS_IN_HOUR);
 });
 
-Deno.test("millisecondsOffset UTC", () => {
-  const offset = millisecondsOffset(new Date("2021-05-13T12:15:30Z"), "UTC");
+Deno.test("tzOffset UTC", () => {
+  const offset = tzOffset(new Date("2021-05-13T12:15:30Z"), "UTC");
   assertEquals(offset, 0);
 });
 
-Deno.test("millisecondsOffset America/New_York DST", () => {
-  const offset = millisecondsOffset(
+Deno.test("tzOffset America/New_York DST", () => {
+  const offset = tzOffset(
     new Date("2021-05-13T12:15:30Z"),
     "America/New_York",
   );
   assertEquals(offset, -(4 * MILLISECONDS_IN_HOUR));
 });
 
-Deno.test("millisecondsOffset America/New_York", () => {
-  const offset = millisecondsOffset(
+Deno.test("tzOffset America/New_York", () => {
+  const offset = tzOffset(
     new Date("2021-12-13T12:15:30Z"),
     "America/New_York",
   );
