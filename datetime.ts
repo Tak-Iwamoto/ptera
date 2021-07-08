ebd5531db4a385eacd70d9afbbaada40f72bcb0e
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-07-09 00:03:03 +0900

    fix add and substract arg

--- datetime.ts
+++ datetime.ts
@@ -309,17 +309,17 @@ export class DateTime {
     return isLeapYear(this.year);
   }
 
-  add(addDateDiff: DateDiff): DateTime {
+  add(diff: DateDiff): DateTime {
     const dt = datetime(
-      adjustedTS(this.toDateObj(), addDateDiff, { positive: true }),
+      adjustedTS(this.toDateObj(), diff, { positive: true }),
       this.#option(),
     );
     return dt;
   }
 
-  substract(subDateObj: Partial<DateObj>): DateTime {
+  substract(diff: DateDiff): DateTime {
     return datetime(
-      adjustedTS(this.toDateObj(), subDateObj, {
+      adjustedTS(this.toDateObj(), diff, {
         positive: false,
       }),
       this.#option(),
